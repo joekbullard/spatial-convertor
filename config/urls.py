581a36fa342dfb95e2915d437baf66aec2d42397
Joe Bullard <joekbullard@gmail.com>
2023-07-09 00:16:27 +0100

    manage.py reformat

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include # new
+from django.urls import path, include  # new
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path("", include("spatial_convertor.urls")),
-    path('accounts/', include('allauth.urls'))
 ]
